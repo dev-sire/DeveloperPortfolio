69773db210fa8b0f60f023b8f53a2aa03a5d944f
Aman Shahid <shahidamaan2019@gmail.com>
2024-02-09 23:49:02 +0500

    deleted workflow

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  // </React.StrictMode>,
+    </HashRouter>
 )
