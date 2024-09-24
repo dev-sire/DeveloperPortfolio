169947cef1a283307cf9ac7c9c06c2e35f90f944
Aman Shahid <shahidamaan2019@gmail.com>
2024-09-24 13:18:51 +0500

    test fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </React.StrictMode>
 )
