bdcebc18e276b40272e189666e958cba277c4d97
dev-sire <shahidamaan2019@gmail.com>
2026-04-27 01:23:23 +0500

    added netlufy config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import envCompatible from 'vite-plugin-env-compatible'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), envCompatible()],
-  base: "/DeveloperPortfolio/"
+  base: "/"
 })
